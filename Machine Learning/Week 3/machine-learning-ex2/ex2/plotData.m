f6401aa884caa769aefd72bd22283f2211871338
Tomasz Jaskula <thomasv1000@hotmail.fr>
2015-09-26 16:47:37 +0200

    Add plotData for Week 3 ex2

--- Machine Learning/Week 3/machine-learning-ex2/ex2/plotData.m
+++ Machine Learning/Week 3/machine-learning-ex2/ex2/plotData.m
@@ -13,11 +13,12 @@ figure; hold on;
 %
 
 
+% Find Indices of Positive and Negative Examples
+pos = find(y==1); neg = find(y == 0);
 
-
-
-
-
+% Plot Examples
+plot(X(pos, 1), X(pos, 2), 'k+','LineWidth', 2, 'MarkerSize', 7);
+plot(X(neg, 1), X(neg, 2), 'ko', 'MarkerFaceColor', 'y', 'MarkerSize', 7);
 
 
 % =========================================================================
