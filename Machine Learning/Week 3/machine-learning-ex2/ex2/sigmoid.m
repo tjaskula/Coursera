4d1832b59f9361a76d2c944882050c1540b95861
Tomasz Jaskula <thomasv1000@hotmail.fr>
2015-09-27 17:01:08 +0200

    Add g function to sigmoid.m

--- Machine Learning/Week 3/machine-learning-ex2/ex2/sigmoid.m
+++ Machine Learning/Week 3/machine-learning-ex2/ex2/sigmoid.m
@@ -9,9 +9,7 @@ g = zeros(size(z));
 % Instructions: Compute the sigmoid of each value of z (z can be a matrix,
 %               vector or scalar).
 
-
-
-
+g = 1 ./ (1 + e .^ -z)
 
 % =============================================================
 
