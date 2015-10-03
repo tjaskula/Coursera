6bc6cf97336053d26c6d86d4356cd4c4dbabf543
Tomasz Jaskula <thomasv1000@hotmail.fr>
2015-10-04 00:14:19 +0200

    Calculate prediction function

--- Machine Learning/Week 3/machine-learning-ex2/ex2/predict.m
+++ Machine Learning/Week 3/machine-learning-ex2/ex2/predict.m
@@ -15,11 +15,13 @@ p = zeros(m, 1);
 %               You should set p to a vector of 0's and 1's
 %
 
-
-
-
-
-
+for i = 1:m
+	if sigmoid(X * theta)(i) >= 0.5
+		p(i) = 1
+	else
+		p(i) = 0
+	endif
+endfor
 
 % =========================================================================
 
