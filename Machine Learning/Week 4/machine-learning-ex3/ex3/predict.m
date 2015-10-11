50264df57f6d413783ed805ce1f2094ada31e50f
Tomasz Jaskula <thomasv1000@hotmail.fr>
2015-10-12 00:06:28 +0200

    Complete prediction with neural network

--- Machine Learning/Week 4/machine-learning-ex3/ex3/predict.m
+++ Machine Learning/Week 4/machine-learning-ex3/ex3/predict.m
@@ -21,13 +21,11 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
-
-
-
-
-
-
-
+a1 = [ones(m, 1) X];
+a2 = [ones(m, 1) sigmoid(a1 * Theta1')];
+a3 = sigmoid(a2 * Theta2');
+[maxA3, imaxA3] = max(a3');
+p = imaxA3';
 
 % =========================================================================
 
