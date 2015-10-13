b185e8f981798b987bb39c2ffa03b7b55465dba2
Thomas Jaskula <thomasv1000@hotmail.fr>
2015-10-13 12:51:31 +0200

    Format the file plotData.m

--- Machine Learning/Week 2/machine-learning-ex1/ex1/plotData.m
+++ Machine Learning/Week 2/machine-learning-ex1/ex1/plotData.m
@@ -16,12 +16,10 @@ function plotData(x, y)
 
 figure; % open a new figure window
 
-
+% x and y are vectors
 plot(x, y, 'rx', 'MarkerSize', 10); % Plot the data
 ylabel('Profit in $10,000s'); % Set the y-axis label
 xlabel('Population of City in 10,000s'); % Set the x-axis label
-
-
 
 % ============================================================
 
