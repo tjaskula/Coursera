c850643f29af66cdd54d63d897c2a2f299996f9d
Thomas Jaskula <thomasv1000@hotmail.fr>
2015-10-14 13:06:09 +0200

    Format computeCost function

--- Machine Learning/Week 2/machine-learning-ex1/ex1/computeCost.m
+++ Machine Learning/Week 2/machine-learning-ex1/ex1/computeCost.m
@@ -13,12 +13,8 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
-
-
 h = X * theta;
-
 squaredErrors = (h - y) .^ 2;
-
 J = (1 / (2 * m)) * sum(squaredErrors);
 
 % =========================================================================
