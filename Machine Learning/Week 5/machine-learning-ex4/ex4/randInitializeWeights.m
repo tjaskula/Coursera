0a08bb95017dd98eceb2169dd6679197344c30e2
Tomasz Jaskula <thomasv1000@hotmail.fr>
2015-10-18 16:56:10 +0200

    Randomly initialize the weights to small values for NN

--- Machine Learning/Week 5/machine-learning-ex4/ex4/randInitializeWeights.m
+++ Machine Learning/Week 5/machine-learning-ex4/ex4/randInitializeWeights.m
@@ -19,7 +19,9 @@ W = zeros(L_out, 1 + L_in);
 % Note: The first row of W corresponds to the parameters for the bias units
 %
 
-
+% Randomly initialize the weights to small values
+epsilon_init = 0.12;
+W = rand(L_out, 1 + L_in) * 2 * epsilon_init - epsilon_init;
 
 % =========================================================================
 
