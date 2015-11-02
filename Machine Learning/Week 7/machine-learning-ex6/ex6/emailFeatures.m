2938bcf1113170d46b63aa50f4f9258ff0f650ad
Tomasz Jaskula <thomasv1000@hotmail.fr>
2015-11-02 02:10:28 +0100

    solve email feature vectorization

--- Machine Learning/Week 7/machine-learning-ex6/ex6/emailFeatures.m
+++ Machine Learning/Week 7/machine-learning-ex6/ex6/emailFeatures.m
@@ -48,7 +48,11 @@ x = zeros(n, 1);
 %
 %
 
+%  x(word_indices)=1;
 
+for i=1:length(word_indices)
+	x(word_indices(i)) = 1;
+end
 
 % =========================================================================
     
