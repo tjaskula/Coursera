6ddd081b88a33e6d7a255208461178b259074d8b
Tomasz Jaskula <thomasv1000@hotmail.fr>
2015-11-02 01:29:08 +0100

    Solve C and Sigma parameters

--- Machine Learning/Week 7/machine-learning-ex6/ex6/dataset3Params.m
+++ Machine Learning/Week 7/machine-learning-ex6/ex6/dataset3Params.m
@@ -23,7 +23,33 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+values = [0.01, 0.03, 0.1, 0.3,  1, 3, 10, 30];
 
+results = [];
+
+l = length(values);
+
+for c=1:l,
+    for sigma=1:l,
+      
+      
+      testC = values(c);
+      testSigma = values(sigma);
+      
+      model= svmTrain(X, y, testC, @(x1, x2) gaussianKernel(x1, x2, testSigma)); 
+      predictions = svmPredict(model, Xval);
+      
+      testError = mean(double(predictions ~= yval));
+      
+      results = [results; testC, testSigma, testError];
+      
+    end
+end
+
+[minError, minIndex] = min(results(:,3));
+
+C = results(minIndex,1);
+sigma = results(minIndex,2);
 
 % =========================================================================
 
