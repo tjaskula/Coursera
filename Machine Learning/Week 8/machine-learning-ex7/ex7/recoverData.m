480e017e64dabbe2d2461bd38fa84f6313df7269
Tomasz Jaskula <thomasv1000@hotmail.fr>
2015-11-08 17:06:03 +0100

    Computing Recover Data (PCA)

--- Machine Learning/Week 8/machine-learning-ex7/ex7/recoverData.m
+++ Machine Learning/Week 8/machine-learning-ex7/ex7/recoverData.m
@@ -21,6 +21,14 @@ X_rec = zeros(size(Z, 1), size(U, 1));
 %               Notice that U(j, 1:K) is a row vector.
 %               
 
+U_reduce = U(:, 1:K);
+
+for i=1:length(Z);
+	v = Z(i, :)';
+	for j=1:length(U_reduce)
+		X_rec(i, j) = v' * U(j, 1:K)';
+	end
+end
 
 % =============================================================
 
