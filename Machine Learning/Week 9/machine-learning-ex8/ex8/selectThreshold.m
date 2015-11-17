ad6e58d9478719b615cd0e102413a01ef9a49e61
Tomasz Jaskula <thomasv1000@hotmail.fr>
2015-11-17 23:23:32 +0100

    fixing bug in select threshold

--- Machine Learning/Week 9/machine-learning-ex8/ex8/selectThreshold.m
+++ Machine Learning/Week 9/machine-learning-ex8/ex8/selectThreshold.m
@@ -23,9 +23,9 @@ for epsilon = min(pval):stepsize:max(pval)
     % Note: You can use predictions = (pval < epsilon) to get a binary vector
     %       of 0's and 1's of the outlier predictions
 
-    cvPredictions = pval < epsilon;
+    cvPredictions = (pval < epsilon);
     fp = sum((cvPredictions == 1) & (yval == 0));
-    tp = sum((cvPredictions == 0) & (yval == 0));
+    tp = sum((cvPredictions == 1) & (yval == 1));
     fn = sum((cvPredictions == 0) & (yval == 1));
 
     prec = tp / (tp + fp);
