f48de79a247f70092aeb8f7d4f07ffb17e33ac00
Tomasz Jaskula <thomasv1000@hotmail.fr>
2015-11-17 22:38:14 +0100

    Calculating mu and sigma2

--- Machine Learning/Week 9/machine-learning-ex8/ex8/estimateGaussian.m
+++ Machine Learning/Week 9/machine-learning-ex8/ex8/estimateGaussian.m
@@ -21,7 +21,8 @@ sigma2 = zeros(n, 1);
 %               should contain variance of the i-th feature.
 %
 
-
+mu = (1 / m) * sum(X);
+sigma2 = (1 / m) * sum((X - mu).^2);
 
 % =============================================================
 
