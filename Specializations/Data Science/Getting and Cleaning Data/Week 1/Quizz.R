b2db5614b6769a4077fc9955c30dd1319329e4c5
Tomasz Jaskula <thomasv1000@hotmail.fr>
2016-01-07 15:08:37 +0100

    Wrapping quizz 1 in the function "question1"

--- Specializations/Data Science/Getting and Cleaning Data/Week 1/Quizz.R
+++ Specializations/Data Science/Getting and Cleaning Data/Week 1/Quizz.R
@@ -1,5 +1,9 @@
-if (!file.exists("data")) {
-  dir.create("data")
+question1 <- function() {
+  
+  if (!file.exists("data")) {
+    dir.create("data")
+  }
+  
+  fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv"
+  download.file(fileUrl, destfile = "./data/cameras.csv", mmethod = "curl")
 }
-
-fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv"
