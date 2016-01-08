0ef155c1eb36d251bde5ac6dee362667c30f0dc2
Tomasz Jaskula <thomasv1000@hotmail.fr>
2016-01-08 15:15:05 +0100

    Refactoring to download and read data function

--- Specializations/Data Science/Getting and Cleaning Data/Week 1/Quizz.R
+++ Specializations/Data Science/Getting and Cleaning Data/Week 1/Quizz.R
@@ -1,9 +1,15 @@
-question1 <- function() {
+question1Download <- function() {
   
   if (!file.exists("data")) {
     dir.create("data")
   }
   
   fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv"
-  download.file(fileUrl, destfile = "./data/cameras.csv", mmethod = "curl")
+  download.file(fileUrl, destfile = "./data/cameras.csv", method = "curl")
+  dateDownloaded <- date()
+}
+
+question1ReadFile <- function() {
+  cameraData <- read.table("./data/cameras.csv", sep = ",", header = TRUE)
+  head(cameraData)
 }
