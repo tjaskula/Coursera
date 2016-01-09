b8b171febf6038bdaef68d9116046a93f052bd6d
Tomasz Jaskula <thomasv1000@hotmail.fr>
2016-01-09 23:50:57 +0100

    Question 3 solution for cleaning data week 1

--- Specializations/Data Science/Getting and Cleaning Data/Week 1/Quizz.R
+++ Specializations/Data Science/Getting and Cleaning Data/Week 1/Quizz.R
@@ -1,3 +1,8 @@
+library(rJava)
+library(xlsxjars)
+library(xlsx)
+library(xml)
+
 question1Download <- function() {
   
   if (!file.exists("data")) {
@@ -18,3 +23,27 @@ question1 <- function() {
   df <- question1ReadFile()
   nrow(df[!is.na(df["VAL"]) & df["VAL"] == 24,])
 }
+
+question3Download <- function() {
+  
+  if (!file.exists("data")) {
+    dir.create("data")
+  }
+  
+  fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FDATA.gov_NGAP.xlsx"
+  download.file(fileUrl, destfile = "./data/NGAP.xlsx", method = "curl")
+  dateDownloaded <- date()
+}
+
+question3ReadFile <- function(rows, cols) {
+  data <- read.xlsx("./data/NGAP.xlsx", sheetIndex = 1, colIndex = cols, rowIndex = rows)
+  data
+}
+
+question3 <- function() {
+  rows <- 18:23
+  cols <- 7:15
+  dat <- question3ReadFile(rows, cols)
+  sum(dat$Zip*dat$Ext,na.rm=T)
+}
+
