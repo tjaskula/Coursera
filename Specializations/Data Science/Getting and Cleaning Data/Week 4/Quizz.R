e4219c3e46548beb712c04a82814416b8c819af8
Tomasz Jaskula <thomasv1000@hotmail.fr>
2016-01-28 00:03:00 +0100

    Solve question 3,4 and 5 from "Getting and cleaning data" Week 4 quizz

--- Specializations/Data Science/Getting and Cleaning Data/Week 4/Quizz.R
+++ Specializations/Data Science/Getting and Cleaning Data/Week 4/Quizz.R
@@ -31,3 +31,33 @@ question2 <- function() {
   df$V5 <- as.numeric(gsub(",", "", df$V5))
   mean(df[1:190, "V5"])
 }
+
+#question 4
+question4 <- function() {
+  
+  if (!file.exists("./data/FGDP.csv")) {
+    download("https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv", "./data/FGDP.csv")
+  }
+  
+  if (!file.exists("./data/FEDSTATS_Country.csv")) {
+    download("https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FEDSTATS_Country.csv", "./data/FEDSTATS_Country.csv")
+  }
+  
+  df <- read.csv("./data/FGDP.csv", sep = ",", header = FALSE, skip = 5)
+  df2 <- read.csv("./data/FEDSTATS_Country.csv", sep = ",", header = TRUE)
+  merged <- merge(df, df2, by.x = "V1", by.y = "CountryCode", all = TRUE)
+  
+  merged <- merged[grep("^Fiscal year.*June.*", merged$Special.Notes),]
+  
+  nrow(merged)
+}
+
+# question 5
+question5 <- function() {
+  library(quantmod)
+  amzn = getSymbols("AMZN",auto.assign=FALSE)
+  filtered <- amzn['2012']
+  lyear <- nrow(filtered)
+  lmon <- nrow(filtered[.indexwday(filtered)==1]) # Mondays
+  c(lyear, lmon)
+}
