49241a8c28be4d1046345ff8dc1a6f1a16c4bb5c
Tomasz Jaskula <thomasv1000@hotmail.fr>
2016-02-22 13:19:50 +0100

    Add initial file for plot2.R

---
+++ Specializations/Data Science/Exploratory Data Analysis/Week 4/plot2.R
@@ -0,0 +1,7 @@
+## Reading data files
+NEI <- readRDS("exdata-data-NEI_data/summarySCC_PM25.rds")
+SCC <- readRDS("exdata-data-NEI_data/Source_Classification_Code.rds")
+
+# Question 2
+# Have total emissions from PM2.5 decreased in the Baltimore City, Maryland (𝚏𝚒𝚙𝚜 == "𝟸𝟺𝟻𝟷𝟶") from 1999 
+# to 2008? Use the base plotting system to make a plot answering this question.
