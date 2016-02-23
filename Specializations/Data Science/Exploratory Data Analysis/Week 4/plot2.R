7b73e86e1f8bb5eae82ef8adb5c70bcb5e19ead8
Tomasz Jaskula <thomasv1000@hotmail.fr>
2016-02-23 13:45:43 +0100

    Add question 2 solution for Exploratory data analysis in week 4

--- Specializations/Data Science/Exploratory Data Analysis/Week 4/plot2.R
+++ Specializations/Data Science/Exploratory Data Analysis/Week 4/plot2.R
@@ -5,3 +5,19 @@ SCC <- readRDS("exdata-data-NEI_data/Source_Classification_Code.rds")
 # Question 2
 # Have total emissions from PM2.5 decreased in the Baltimore City, Maryland (𝚏𝚒𝚙𝚜 == "𝟸𝟺𝟻𝟷𝟶") from 1999 
 # to 2008? Use the base plotting system to make a plot answering this question.
+
+isBaltimore <- NEI$fips == "24510"
+baltimore <- subset(NEI, isBaltimore)
+rm(isBalt
+imore)
+
+totalEmissionByYear <- with(NEI, tappbaltimoreEmissions, year, sum, na.rm = TRUE))
+
+plot(x = names(totalEmissionByYear), y = totalEmissionByYear, 
+     type = "p", pch = 16, col = "green", axes = FALSE,
+     ylab = "Total PM2.5 Emission (in tons)", xlab = "Year", 
+     main = "Total PM2.5 Emission from 1999 to 2008")
+lines(x = names(totalEmissionByYear), y = totalEmissionByYear, col = "blue")
+axis(2) # x axis
+axis(side = 1, at = names(totalEmissionByYear)) # y axis
+box()
