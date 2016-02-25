48d078b63d96cba519919a0a48832b087da46032
Tomasz Jaskula <thomasv1000@hotmail.fr>
2016-02-25 13:54:54 +0100

    Add initial plot3.R script

---
+++ Specializations/Data Science/Exploratory Data Analysis/Week 4/plot3.R
@@ -0,0 +1,9 @@
+## Reading data files
+NEI <- readRDS("exdata-data-NEI_data/summarySCC_PM25.rds")
+SCC <- readRDS("exdata-data-NEI_data/Source_Classification_Code.rds")
+
+# Question 3
+# Of the four types of sources indicated by the 𝚝𝚢𝚙𝚎 (point, nonpoint, onroad, nonroad) variable, 
+# which of these four sources have seen decreases in emissions from 1999–2008 for Baltimore City? 
+# Which have seen increases in emissions from 1999–2008? 
+# Use the ggplot2 plotting system to make a plot answer this question.
