6926940503e956182f4d319822ecf61ceba1fee5
Tomasz Jaskula <thomasv1000@hotmail.fr>
2016-02-26 14:52:12 +0100

    Add plot2.R solution for Exploratory data analysis week 4

--- Specializations/Data Science/Exploratory Data Analysis/Week 4/plot3.R
+++ Specializations/Data Science/Exploratory Data Analysis/Week 4/plot3.R
@@ -1,3 +1,5 @@
+library(ggplot2)
+
 ## Reading data files
 NEI <- readRDS("exdata-data-NEI_data/summarySCC_PM25.rds")
 SCC <- readRDS("exdata-data-NEI_data/Source_Classification_Code.rds")
@@ -7,3 +9,15 @@ SCC <- readRDS("exdata-data-NEI_data/Source_Classification_Code.rds")
 # which of these four sources have seen decreases in emissions from 1999–2008 for Baltimore City? 
 # Which have seen increases in emissions from 1999–2008? 
 # Use the ggplot2 plotting system to make a plot answer this question.
+
+isBaltimore <- NEI$fips == "24510"
+baltimore <- subset(NEI, isBaltimore)
+rm(isBaltimore)
+
+totalEmissionByYearAndType <- aggregate(Emissions ~ year + type, data = baltimore, sum)
+
+qplot(x = year, y = Emissions, data = totalEmissionByYearAndType, col = type, facets = . ~ type) + 
+  geom_line() +
+  labs(title = "Total PM2.5 Emission from 1999 to 2008 for Baltimore") +
+  labs(x = "Year", y = "Total PM2.5 Emission (in tons)") +
+  scale_x_continuous(breaks = unique(totalEmissionByYearAndType$year))
