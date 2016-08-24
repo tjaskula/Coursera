42b744a8c4fa88691771917c6b8f5297853b770b
Tomasz Jaskula <thomasv1000@hotmail.fr>
2016-08-25 01:14:50 +0200

    Scala assignment week 3

---
+++ Specializations/Functional Programming in Scala/Functional Programming Principles in Scala/Week 3/objsets/build.sbt
@@ -0,0 +1,73 @@
+name := course.value + "-" + assignment.value
+
+scalaVersion := "2.11.7"
+
+scalacOptions ++= Seq("-deprecation")
+
+// grading libraries
+libraryDependencies += "junit" % "junit" % "4.10" % "test"
+
+// for funsets
+libraryDependencies += "org.scala-lang.modules" %% "scala-parser-combinators" % "1.0.4"
+
+// include the common dir
+commonSourcePackages += "common"
+
+courseId := "bRPXgjY9EeW6RApRXdjJPw"
+
+// See documentation in ProgFunBuild.scala
+assignmentsMap := {
+  val styleSheetPath = (baseDirectory.value / ".." / ".." / "project" / "scalastyle_config.xml").getPath
+  Map(
+    "example" -> Assignment(
+      packageName = "example",
+      key = "g4unnjZBEeWj7SIAC5PFxA",
+      itemId = "xIz9O",
+      partId = "d5jxI",
+      maxScore = 10d,
+      styleScoreRatio = 0.2,
+      styleSheet = styleSheetPath),
+    "recfun" -> Assignment(
+      packageName = "recfun",
+      key = "SNYuDzZEEeWNVyIAC92BaQ",
+      itemId = "Ey6Jf",
+      partId = "PzVVY",
+      maxScore = 10d,
+      styleScoreRatio = 0.2,
+      styleSheet = styleSheetPath),
+    "funsets" -> Assignment(
+      packageName = "funsets",
+      key = "FNHHMDfsEeWAGiIAC46PTg",
+      itemId = "BVa6a",
+      partId = "IljBE",
+      maxScore = 10d,
+      styleScoreRatio = 0.2,
+      styleSheet = styleSheetPath),
+    "objsets" -> Assignment(
+      packageName = "objsets",
+      key = "6PTXvD99EeWAiCIAC7Pj9w",
+      itemId = "Ogg05",
+      partId = "7hlkb",
+      maxScore = 10d,
+      styleScoreRatio = 0.2,
+      styleSheet = styleSheetPath,
+      options = Map("grader-timeout" -> "1800")),
+    "patmat" -> Assignment(
+      packageName = "patmat",
+      key = "BwkTtD9_EeWFZSIACtiVgg",
+      itemId = "uctOq",
+      partId = "2KYZc",
+      maxScore = 10d,
+      styleScoreRatio = 0.2,
+      styleSheet = styleSheetPath),
+    "forcomp" -> Assignment(
+      packageName = "forcomp",
+      key = "CPJe397VEeWLGArWOseZkw",
+      itemId = "nVRPb",
+      partId = "v2XIe",
+      maxScore = 10d,
+      styleScoreRatio = 0.2,
+      styleSheet = styleSheetPath,
+      options = Map("grader-timeout" -> "1800"))
+  )
+}
---
+++ Specializations/Functional Programming in Scala/Functional Programming Principles in Scala/Week 3/objsets/src/test/scala/objsets/TweetSetSuite.scala
@@ -0,0 +1,98 @@
+package objsets
+
+import org.scalatest.FunSuite
+
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class TweetSetSuite extends FunSuite {
+  trait TestSets {
+    val set1 = new Empty
+    val set2 = set1.incl(new Tweet("a", "a body", 20))
+    val set3 = set2.incl(new Tweet("b", "b body", 20))
+    val c = new Tweet("c", "c body", 7)
+    val d = new Tweet("d", "d body", 9)
+    val e = new Tweet("e", "e body", 22)
+    val set4c = set3.incl(c)
+    val set4d = set3.incl(d)
+    val set5 = set4c.incl(d)
+    val set6 = set2.incl(c).incl(d).incl(e)
+  }
+
+  def asSet(tweets: TweetSet): Set[Tweet] = {
+    var res = Set[Tweet]()
+    tweets.foreach(res += _)
+    res
+  }
+
+  def size(set: TweetSet): Int = asSet(set).size
+
+  test("filter: on empty set") {
+    new TestSets {
+      assert(size(set1.filter(tw => tw.user == "a")) === 0)
+    }
+  }
+
+  test("filter: a on set5") {
+    new TestSets {
+      assert(size(set5.filter(tw => tw.user == "a")) === 1)
+    }
+  }
+
+  test("filter: 20 on set5") {
+    new TestSets {
+      assert(size(set5.filter(tw => tw.retweets == 20)) === 2)
+    }
+  }
+
+  test("union: set4c and set4d") {
+    new TestSets {
+      assert(size(set4c.union(set4d)) === 4)
+    }
+  }
+
+  test("union: with empty set (1)") {
+    new TestSets {
+      assert(size(set5.union(set1)) === 4)
+    }
+  }
+
+  test("union: with empty set (2)") {
+    new TestSets {
+      assert(size(set1.union(set5)) === 4)
+    }
+  }
+
+  test("mostRetweeted: with set (6)") {
+    new TestSets {
+      assert(set6.mostRetweeted.retweets === 22)
+    }
+  }
+
+  test("mostRetweeted: with set (3)") {
+    new TestSets {
+      val mostRetweeted = set3.mostRetweeted
+      assert(mostRetweeted.retweets === 20)
+      assert(mostRetweeted.user === "b")
+    }
+  }
+
+  test("mostRetweeted: with set (5)") {
+    new TestSets {
+      val mostRetweeted = set5.mostRetweeted
+      assert(mostRetweeted.retweets === 20)
+      assert(mostRetweeted.user === "b")
+    }
+  }
+
+  test("descending: set5") {
+    new TestSets {
+      val trends = set5.descendingByRetweet
+      assert(!trends.isEmpty)
+      assert(trends.head.user == "a" || trends.head.user == "b")
+    }
+  }
+
+  }
