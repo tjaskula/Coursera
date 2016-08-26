88786c6c9d9ec1a7a94766a97de615a28a525eff
Tomasz Jaskula <thomasv1000@hotmail.fr>
2016-08-26 22:04:01 +0200

    Add maxpairwise product Haskell

--- Specializations/Data Structures and Algorithms/Algorithmic Toolbox/Week 1/MaxPairwiseProduct.hs
+++ Specializations/Data Structures and Algorithms/Algorithmic Toolbox/Week 1/MaxPairwiseProduct.hs
@@ -1,5 +1,35 @@
-l5 = [100000, 90000]
+import System.IO
+import Control.Monad
 
-findTwoMax l = l * l
+main :: IO ()
+main = do
+    hSetBuffering stdout NoBuffering -- DO NOT REMOVE
+    
+    -- Auto-generated code below aims at helping you parse
+    -- the standard input according to the problem statement.
+    input_line <- getLine
+    input_line <- getLine
 
-res = findTwoMax 5
+    let split :: Eq a => a -> [a] -> [[a]]
+        split d [] = []
+        split d s = x : split d (drop 1 y) where (x,y) = span (/= d) s
+
+    let splitted = split ' ' input_line
+    let numbers = map (read::String->Int) splitted
+
+    let maximum2 :: Ord a => [a] -> (a,a)
+        maximum2 [] = undefined
+        maximum2 [x] = undefined
+        maximum2 [x1, x2] = (x1, x2)
+        maximum2 (x1 : x2 : x3 : xs)
+          | x2 > x1 = maximum2 (x2 : x1 : x3 : xs)
+          | x3 > x1 = maximum2 (x3 : x1 : xs)
+          | x3 > x2 = maximum2 (x1 : x3 : xs)
+          | otherwise = maximum2 (x1 : x2 : xs)
+
+    let tuples = maximum2 numbers
+    let res = (fst tuples) * (snd tuples)
+
+    putStrLn (show res)
+
+    return ()
