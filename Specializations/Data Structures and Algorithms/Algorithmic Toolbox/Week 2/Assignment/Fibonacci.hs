1dd15611b1e72ec1fb1a29deee6aed116717882a
Tomasz Jaskula <thomasv1000@hotmail.fr>
2016-08-28 01:08:55 +0200

    Add starter file for haskell

---
+++ Specializations/Data Structures and Algorithms/Algorithmic Toolbox/Week 2/Assignment/Fibonacci.hs
@@ -0,0 +1,20 @@
+import System.IO
+import Control.Monad
+
+main :: IO ()
+main = do
+    hSetBuffering stdout NoBuffering
+
+    input_line <- getLine
+    let readInt :: String -> Int
+        readInt = read
+    let n = readInt input_line
+    
+    let fibonacci 0 = 0
+        fibonacci 1 = 1
+        fibonacci n = fibonacci(n - 1) + fibonacci(n - 2)
+
+    let res = fibonacci n
+    putStrLn $ show res
+
+    return ()
