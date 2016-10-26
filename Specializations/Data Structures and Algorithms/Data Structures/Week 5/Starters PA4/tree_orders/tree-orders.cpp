50c58b7f35575dac459aaf9590547948ed7d5406
Tomasz Jaskula <thomasv1000@hotmail.fr>
2016-10-26 19:10:52 +0200

    Add Starter files to week 5 DataStructures

---
+++ Specializations/Data Structures and Algorithms/Data Structures/Week 5/Starters PA4/tree_orders/tree-orders.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using std::vector;
+using std::ios_base;
+using std::cin;
+using std::cout;
+
+class TreeOrders {
+  int n;
+  vector <int> key;
+  vector <int> left;
+  vector <int> right;
+
+public:
+  void read() {
+    cin >> n;
+    key.resize(n);
+    left.resize(n);
+    right.resize(n);
+    for (int i = 0; i < n; i++) {
+      cin >> key[i] >> left[i] >> right[i];
+    }
+  }
+
+
+  vector <int> in_order() {
+    vector<int> result;
+    // Finish the implementation
+    // You may need to add a new recursive method to do that
+
+    return result;
+  }
+
+  vector <int> pre_order() {
+    vector<int> result;    
+    // Finish the implementation
+    // You may need to add a new recursive method to do that
+    
+    return result;
+  }
+
+  vector <int> post_order() {
+    vector<int> result;
+    // Finish the implementation
+    // You may need to add a new recursive method to do that
+    
+    return result;
+  }
+};
+
+void print(vector <int> a) {
+  for (size_t i = 0; i < a.size(); i++) {
+    if (i > 0) {
+      cout << ' ';
+    }
+    cout << a[i];
+  }
+  cout << '\n';
+}
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  TreeOrders t;
+  t.read();
+  print(t.in_order());
+  print(t.pre_order());
+  print(t.post_order());
+  return 0;
+}
