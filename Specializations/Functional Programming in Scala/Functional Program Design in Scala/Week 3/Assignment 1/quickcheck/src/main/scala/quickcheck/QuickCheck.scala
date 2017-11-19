c5c6e12d707d244a6e884a283b15b0e6385de324
Tomasz Jaskula <thomasv1000@hotmail.fr>
2017-11-19 21:06:27 +0100

    Generator function for scala

--- Specializations/Functional Programming in Scala/Functional Program Design in Scala/Week 3/Assignment 1/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ Specializations/Functional Programming in Scala/Functional Program Design in Scala/Week 3/Assignment 1/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -9,7 +9,11 @@ import Prop._
 
 abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
 
-  lazy val genHeap: Gen[H] = ???
+  lazy val genHeap: Gen[H] = for {
+    element <- arbitrary[Int]
+    heap <- oneOf(const(empty), genHeap)
+  } yield insert(element, heap)
+
   implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
 
   property("gen1") = forAll { (h: H) =>
