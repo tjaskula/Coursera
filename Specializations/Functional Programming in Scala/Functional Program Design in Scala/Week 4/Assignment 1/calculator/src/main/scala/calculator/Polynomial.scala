5c93e334305ee388f4dff0493ded02069a015b43
Tomasz Jaskula <thomasv1000@hotmail.fr>
2018-05-19 23:34:32 +0200

    Add initial assignment week 4

---
+++ Specializations/Functional Programming in Scala/Functional Program Design in Scala/Week 4/Assignment 1/calculator/src/main/scala/calculator/Polynomial.scala
@@ -0,0 +1,13 @@
+package calculator
+
+object Polynomial {
+  def computeDelta(a: Signal[Double], b: Signal[Double],
+      c: Signal[Double]): Signal[Double] = {
+    ???
+  }
+
+  def computeSolutions(a: Signal[Double], b: Signal[Double],
+      c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
+    ???
+  }
+}
