28674606dd655600b4df301189a0c1abd7f3c013
Tomasz Jaskula <thomasv1000@hotmail.fr>
2018-05-23 00:25:02 +0200

    Polynomial solver

--- Specializations/Functional Programming in Scala/Functional Program Design in Scala/Week 4/Assignment 1/calculator/src/main/scala/calculator/Polynomial.scala
+++ Specializations/Functional Programming in Scala/Functional Program Design in Scala/Week 4/Assignment 1/calculator/src/main/scala/calculator/Polynomial.scala
@@ -1,13 +1,20 @@
 package calculator
 
+import scala.math.{pow, sqrt}
+
 object Polynomial {
   def computeDelta(a: Signal[Double], b: Signal[Double],
       c: Signal[Double]): Signal[Double] = {
-    ???
+    new Signal[Double](pow(b(), 2) - 4 * a() * c())
   }
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
       c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
-    ???
+    Signal {
+      delta() match {
+        case d if d < 0 => Set()
+        case _ => Set((-b() + sqrt(delta())) / (2 * a()), (-b() - sqrt(delta())) / (2 * a()))
+      }
+    }
   }
 }
