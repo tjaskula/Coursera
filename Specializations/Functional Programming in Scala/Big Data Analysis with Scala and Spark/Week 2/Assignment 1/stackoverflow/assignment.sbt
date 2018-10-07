e26bf8f3db25d82e6fd2622cf0aa262bbba89a20
Tomasz Jaskula <thomasv1000@hotmail.fr>
2018-10-07 10:36:18 +0200

    Add initial sources

---
+++ Specializations/Functional Programming in Scala/Big Data Analysis with Scala and Spark/Week 2/Assignment 1/stackoverflow/assignment.sbt
@@ -0,0 +1,10 @@
+course := "bigdata"
+assignment := "stackoverflow"
+
+assignmentInfo := AssignmentInfo(
+  key = "7ByAoS4kEea1yxIfJA1CUw",
+  itemId = "QhzMw",
+  premiumItemId = Some("FWGnz"),
+  partId = "OY5fJ",
+  styleSheet = None
+)
---
+++ Specializations/Functional Programming in Scala/Big Data Analysis with Scala and Spark/Week 2/Assignment 1/stackoverflow/src/test/scala/stackoverflow/StackOverflowSuite.scala
@@ -0,0 +1,38 @@
+package stackoverflow
+
+import org.scalatest.{FunSuite, BeforeAndAfterAll}
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.rdd.RDD
+import java.io.File
+
+@RunWith(classOf[JUnitRunner])
+class StackOverflowSuite extends FunSuite with BeforeAndAfterAll {
+
+
+  lazy val testObject = new StackOverflow {
+    override val langs =
+      List(
+        "JavaScript", "Java", "PHP", "Python", "C#", "C++", "Ruby", "CSS",
+        "Objective-C", "Perl", "Scala", "Haskell", "MATLAB", "Clojure", "Groovy")
+    override def langSpread = 50000
+    override def kmeansKernels = 45
+    override def kmeansEta: Double = 20.0D
+    override def kmeansMaxIterations = 120
+  }
+
+  test("testObject can be instantiated") {
+    val instantiatable = try {
+      testObject
+      true
+    } catch {
+      case _: Throwable => false
+    }
+    assert(instantiatable, "Can't instantiate a StackOverflow object")
+  }
+
+
+}
