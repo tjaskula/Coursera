007018524a65b16b9a71f004133b1f7fdb93d351
Tomasz Jaskula <thomasv1000@hotmail.fr>
2018-10-20 22:31:37 +0200

    Defining dfSchema

--- Specializations/Functional Programming in Scala/Big Data Analysis with Scala and Spark/Week 4/Assignment 1/timeusage/src/main/scala/timeusage/TimeUsage.scala
+++ Specializations/Functional Programming in Scala/Big Data Analysis with Scala and Spark/Week 4/Assignment 1/timeusage/src/main/scala/timeusage/TimeUsage.scala
@@ -62,8 +62,11 @@ object TimeUsage {
     *         have type Double. None of the fields are nullable.
     * @param columnNames Column names of the DataFrame
     */
-  def dfSchema(columnNames: List[String]): StructType =
-    ???
+  def dfSchema(columnNames: List[String]): StructType = {
+    val first = StructField(columnNames.head, StringType, false)
+    val rest = columnNames.tail map(columnName => StructField(columnName, DoubleType, false))
+    StructType(first +: rest)
+  }
 
 
   /** @return An RDD Row compatible with the schema produced by `dfSchema`
