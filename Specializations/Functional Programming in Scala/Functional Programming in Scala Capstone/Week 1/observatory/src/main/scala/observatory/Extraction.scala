78f1e2b2a7825a4e4d9fc70139c3092b26ceb673
Tomasz Jaskula <thomasv1000@hotmail.fr>
2018-10-27 19:50:27 +0200

    Working on Extraction assignment

--- Specializations/Functional Programming in Scala/Functional Programming in Scala Capstone/Week 1/observatory/src/main/scala/observatory/Extraction.scala
+++ Specializations/Functional Programming in Scala/Functional Programming in Scala Capstone/Week 1/observatory/src/main/scala/observatory/Extraction.scala
@@ -1,11 +1,30 @@
 package observatory
 
+import java.io.File
+import java.nio.file.Paths
 import java.time.LocalDate
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SparkSession
 
 /**
   * 1st milestone: data extraction
   */
 object Extraction {
+
+  import org.apache.log4j.{Level, Logger}
+  Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
+
+  val spark: SparkSession =
+    SparkSession
+      .builder()
+      .appName("Observatory")
+      .config("spark.master", "local")
+      .getOrCreate()
+
+  /** @return The filesystem path of the given resource */
+  def fsPath(resource: String): String =
+    Paths.get(getClass.getResource(resource).toURI).toString
 
   /**
     * @param year             Year number
@@ -14,7 +33,29 @@ object Extraction {
     * @return A sequence containing triplets (date, location, temperature)
     */
   def locateTemperatures(year: Year, stationsFile: String, temperaturesFile: String): Iterable[(LocalDate, Location, Temperature)] = {
-    ???
+    sparkLocateTemperatures(year, stationsFile, temperaturesFile).collect()
+  }
+
+  def sparkLocateTemperatures(year: Year, stationsFile: String, temperaturesFile: String): RDD[(LocalDate, Location, Temperature)] = {
+    def toStationsPairs(line: String): ((String, String), Location) = {
+      val (stn, wban, lat, lon) = line.split(",", -1) match { case Array(x, y, w, z) => (x, y, w, z) }
+      ((stn, wban), Location(if (lat == "") 0.0 else lat.toDouble, if (lon == "") 0.0 else lon.toDouble))
+    }
+
+    def toTemperaturePairs(line: String): ((String, String), (Int, Int, Temperature)) = {
+      val (stn, wban, month, day, temp) = line.split(",", -1) match { case Array(x, y, v, w, z) => (x, y, v, w, z) }
+      ((stn, wban), (month.toInt, day.toInt, (temp.toDouble - 32) * (5/9)))
+    }
+
+    val stationsRdd = spark.sparkContext.textFile(fsPath(stationsFile))
+      .map(toStationsPairs)
+      .filter(stationPairs => stationPairs._2.lat != 0.0 && stationPairs._2.lon != 0.0)
+
+    val temperaturesRdd = spark.sparkContext.textFile(fsPath(temperaturesFile)).map(toTemperaturePairs)
+
+    stationsRdd
+      .join(temperaturesRdd)
+      .map(p => (LocalDate.of(year, p._2._2._1, p._2._2._2), p._2._1, p._2._2._3))
   }
 
   /**
@@ -22,7 +63,14 @@ object Extraction {
     * @return A sequence containing, for each location, the average temperature over the year.
     */
   def locationYearlyAverageRecords(records: Iterable[(LocalDate, Location, Temperature)]): Iterable[(Location, Temperature)] = {
-    ???
+    sparkAverageRecords(spark.sparkContext.parallelize(records.toSeq)).collect().toSeq
   }
 
+  // Added method:
+  def sparkAverageRecords(records: RDD[(LocalDate, Location, Temperature)]): RDD[(Location, Temperature)] = {
+    records
+      .map(record => (record._2, (record._3, 1.0)))
+      .reduceByKey((r1, r2) => (r1._1 + r2._1, r1._2 + r2._2))
+      .mapValues({case (temp, count) => temp / count})
+  }
 }
--- Specializations/Functional Programming in Scala/Functional Programming in Scala Capstone/Week 1/observatory/src/test/scala/observatory/ExtractionTest.scala
+++ Specializations/Functional Programming in Scala/Functional Programming in Scala Capstone/Week 1/observatory/src/test/scala/observatory/ExtractionTest.scala
@@ -1,10 +1,33 @@
 package observatory
 
 import org.junit.runner.RunWith
-import org.scalatest.FunSuite
+import org.scalatest.{BeforeAndAfterAll, FunSuite}
 import org.scalatest.junit.JUnitRunner
 
-trait ExtractionTest extends FunSuite {
+@RunWith(classOf[JUnitRunner])
+class ExtractionTest extends FunSuite  with BeforeAndAfterAll {
+  def initializeExtraction(): Boolean =
+    try {
+      Extraction
+      true
+    } catch {
+      case ex: Throwable =>
+        println(ex.getMessage)
+        ex.printStackTrace()
+        false
+    }
 
-  
+  override def afterAll(): Unit = {
+    assert(initializeExtraction(), " -- did you fill in all the values in Extraction (conf, sc, wikiRdd)?")
+    import Extraction._
+    spark.stop()
+  }
+
+  test("'occurrencesOfLang' should work for (specific) RDD with one element") {
+//    assert(initializeExtraction(), " -- did you fill in all the values in WikipediaRanking (conf, sc, wikiRdd)?")
+//    import WikipediaRanking._
+//    val rdd = sc.parallelize(Seq(WikipediaArticle("title", "Java Jakarta")))
+//    val res = (occurrencesOfLang("Java", rdd) == 1)
+//    assert(res, "occurrencesOfLang given (specific) RDD with one element should equal to 1")
+  }
 }
